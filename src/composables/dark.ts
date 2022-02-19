c6c40d0d6e66b157ce933b95a5730aff0d5f25f1
Johan Vergeer <johanvergeer@gmail.com>
2022-02-19 19:10:48 +0100

    Set default theme to light

--- src/composables/dark.ts
+++ src/composables/dark.ts
@@ -1,3 +1,3 @@
 // these APIs are auto-imported from @vueuse/core
-export const isDark = useDark()
+export const isDark = useDark({ attribute: "color-scheme" })
 export const toggleDark = useToggle(isDark)
